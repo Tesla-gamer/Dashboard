323456f3fbe32beb8ff462e1901ac813621959ae
Abhishek <guptaabhishek46053@gmail.com>
2021-12-07 13:58:45 +0530

    added splash screen

--- app/src/main/java/com/example/bandmast/Dashboard.java
+++ app/src/main/java/com/example/bandmast/Dashboard.java
@@ -1,8 +1,10 @@
 package com.example.bandmast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
@@ -12,13 +14,15 @@ public class Dashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setStatusBarColor(ContextCompat.getColor(Dashboard.this, R.color.colorAccent));
         setContentView(R.layout.activity_dashboard);
         handler=new Handler();
         handler.postDelayed(() -> {
             Intent intent=new Intent(Dashboard.this,MainActivity2.class);
             startActivity(intent);
             finish();
-        },3000);
+        },5000);
     }
 }
--- app/src/main/java/com/example/bandmast/MainActivity2.java
+++ app/src/main/java/com/example/bandmast/MainActivity2.java
@@ -3,9 +3,9 @@ package com.example.bandmast;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity2 extends AppCompatActivity {
     Handler handler;
@@ -14,7 +14,8 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity2.this, R.color.colorAccent));
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         handler = new Handler();
         handler.postDelayed(() -> {
             Intent intent = new Intent(MainActivity2.this, MainActivity.class);
